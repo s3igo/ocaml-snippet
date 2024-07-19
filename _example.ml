10b17ca79931439557f20509282bc131a38bd5ef
s3igo <85787242+s3igo@users.noreply.github.com>
2024-07-20 03:29:41 +0900

    feat: update example

--- _example.ml
+++ _example.ml
@@ -10,8 +10,8 @@ let hoge x =
 
 let fake x = x + 1 [@@fake_attr]
 
-type fuga = string [@@snippet]
+type fuga = string [@@snippet ";fuga"]
 
-let piyo = "piyo" [@@snippet]
+let piyo = "piyo" [@@snippet piyo]
 let my_function x = x + 1
 let () = my_function 2 |> Printf.printf "%d"
