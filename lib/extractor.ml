98aec14132d4c41ecf600cb7a52db13df6077cb4
s3igo <85787242+s3igo@users.noreply.github.com>
2024-07-21 13:45:10 +0900

    feat: split parser.ml

--- lib/extractor.ml
+++ lib/extractor.ml
@@ -1,2 +1,24 @@
-(*open Types*)
+let extract_source_code filename (loc : Ppxlib.location) =
+  let ic = open_in filename in
+  let len = loc.loc_end.pos_cnum - loc.loc_start.pos_cnum in
+  let buf = Bytes.create len in
+  seek_in ic loc.loc_start.pos_cnum;
+  really_input ic buf 0 len;
+  close_in ic;
+  let code = Bytes.to_string buf in
+  (* Remove the [@@snippet payload] attribute *)
+  let regex = Str.regexp "\\[@@snippet[^]]*\\]" in
+  Str.global_replace regex "" code |> String.trim
 
+
+(** Extract payload from an attribute
+    @param payload The attribute payload
+    @return An option containing the extracted payload string, or None if extraction fails
+*)
+let extract_payload = function
+  | Ppxlib.PStr [ { pstr_desc = Pstr_eval (expr, _); _ } ] -> (
+      match expr with
+      | { pexp_desc = Pexp_constant (Pconst_string (s, _, _)); _ } -> Some s
+      | { pexp_desc = Pexp_ident { txt = Lident s; _ }; _ } -> Some s
+      | _ -> None)
+  | _ -> None
--- lib/parser.ml
+++ lib/parser.ml
@@ -1,29 +1,4 @@
-let extract_source_code filename (loc : Ppxlib.location) =
-  let ic = open_in filename in
-  let len = loc.loc_end.pos_cnum - loc.loc_start.pos_cnum in
-  let buf = Bytes.create len in
-  seek_in ic loc.loc_start.pos_cnum;
-  really_input ic buf 0 len;
-  close_in ic;
-  let code = Bytes.to_string buf in
-  (* Remove the [@@snippet payload] attribute *)
-  let regex = Str.regexp "\\[@@snippet[^]]*\\]" in
-  Str.global_replace regex "" code |> String.trim
-
-(** Extract payload from an attribute
-    @param payload The attribute payload
-    @return An option containing the extracted payload string, or None if extraction fails
-*)
-let extract_payload = function
-  | Ppxlib.PStr [ { pstr_desc = Pstr_eval (expr, _); _ } ] -> (
-      match expr with
-      | { pexp_desc = Pexp_constant (Pconst_string (s, _, _)); _ } -> Some s
-      | { pexp_desc = Pexp_ident { txt = Lident s; _ }; _ } -> Some s
-      | _ -> None)
-  | _ -> None
-
 [@@@ocamlformat "disable"]
-
 let rec find_items filename =
   let module LocalTypes = Types in
   let open Ppxlib in
@@ -38,8 +13,8 @@ let rec find_items filename =
           | Ppat_var { txt = _; _ } ->
               vb.pvb_attributes |> List.find_map (fun (attr : attribute) ->
                 if String.equal attr.attr_name.txt "snippet" then
-                  match extract_payload attr.attr_payload with
-                  | Some payload -> Some (LocalTypes.Let_binding (extract_source_code filename loc, payload))
+                  match Extractor.extract_payload attr.attr_payload with
+                  | Some payload -> Some (LocalTypes.Let_binding (Extractor.extract_source_code filename loc, payload))
                   | None -> None
                 else
                   None)
@@ -51,8 +26,8 @@ let rec find_items filename =
         type_decls |> List.filter_map (fun td ->
           td.ptype_attributes |> List.find_map (fun (attr : attribute) ->
             if String.equal attr.attr_name.txt "snippet" then
-              match extract_payload attr.attr_payload with
-              | Some payload -> Some (LocalTypes.Type_decl (extract_source_code filename loc, payload))
+              match Extractor.extract_payload attr.attr_payload with
+              | Some payload -> Some (LocalTypes.Type_decl (Extractor.extract_source_code filename loc, payload))
               | None -> None
             else
               None))
